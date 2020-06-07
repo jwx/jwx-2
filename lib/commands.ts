e40da6bbf65d05350e9c2277c87aa48b581c0b29
Jürgen Wenzel <jurgen@jwenzel.se>
2020-06-07 11:26:25 +0200

    feat(jwx-2-cli): initial commit

---
+++ lib/commands.ts
@@ -0,0 +1,26 @@
+export default {
+    new: {
+        cmd: 'npx',
+        args: ['makes', 'aurelia'],
+        passThroughArgs: true,
+        requiresProject: false,
+    },
+    help: {
+        cmd: 'help',
+        args: [],
+        passThroughArgs: true,
+        requiresProject: false,
+    },
+    localize: {
+        cmd: 'npm',
+        args: ['install', '--save-dev', 'jwx-2'],
+        passThroughArgs: false,
+        requiresProject: true,
+    },
+    globalize: {
+        cmd: 'npm',
+        args: ['install', '-g', 'jwx-2'],
+        passThroughArgs: false,
+        requiresProject: false,
+    },
+};
